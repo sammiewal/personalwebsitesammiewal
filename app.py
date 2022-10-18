c8f33cb8c0eee977df6ad0ba2c7c4dc0f438fd1d
Ubuntu <ubuntu@ip-172-31-21-141.us-west-2.compute.internal>
2022-10-18 02:21:16 +0000

    commit msg

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
-from flask import Flask
+from datetime import date
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    today = todays_date()
+    return render_template("index.html", the_date = today)
+    
+@app.route("/about")
+def about_me():
+    return render_template("about_me.html")
+    
+    
+def todays_date():
+    today = date.today()
+    str_date = today.strftime("%B %d, %Y")
+    return "Today's date is " + str_date
